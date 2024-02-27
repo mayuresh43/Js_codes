30882c6627227775bd9cf0b64da06d22739abf75
Mayuresh Rajendra Naik <94290797+mayuresh43@users.noreply.github.com>
2024-02-27 11:22:04 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1254
+let accountEmail = "mayuresh@google.com"
+var accountPass = "1289" 
+let accountState
+
+// prefer not use var bcoz of issue in block scope functional scope.
+
+console.table([accountId, accountEmail, accountPass, accountState])
